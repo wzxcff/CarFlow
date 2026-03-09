fac2b9adc0d98db54d1011570b4475dc1b871016
Bohdan Shevchenko <Shevasfloyd@gmail.com>
2026-03-09 16:10:38 +0100

    init

---
+++ src/main/java/org/carflow/carflow/CarFlowApplication.java
@@ -0,0 +1,13 @@
+package org.carflow.carflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carflow/carflow/CarFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carflow.carflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
